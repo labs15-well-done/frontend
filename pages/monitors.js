8d9cf63ef414e67191f89a789e76537f1aefd9f7
Andrew Brudnak <brudnak@icloud.com>
2019-08-21 08:47:32 -0700

    :nail_care: Setup eslint-config-airbnb

--- pages/monitors.js
+++ pages/monitors.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import Nav from '../components/Nav';
+import React from 'react';
+import Nav from '../components/nav';
 
 export default function monitors() {
-    return (
-        <>
-            <div className='container'>
-                <Nav />
-                <h2>Monitors Page</h2>
-            </div>
-            <style jsx>{`
-            .container {
-                display: flex;
-            }
-            h2 {
-                margin-left: 10px;
-              }
-    `}</style>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <Nav />
+        <h2>Monitors Page</h2>
+      </div>
+      <style jsx>
+        {`
+          .container {
+            display: flex;
+          }
+          h2 {
+            margin-left: 10px;
+          }
+        `}
+      </style>
+    </>
+  );
 }
