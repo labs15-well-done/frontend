a0fdaa29f6c91814910b7c967b019922994655f9
Maks <maks112v@gmail.com>
2019-08-23 01:52:13 +0300

    added something

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,15 +8,45 @@ import {
   FiSettings,
 } from "react-icons/fi"
 import { colors } from "./Styles"
+import Joyride from "react-joyride"
 
 const nav = [
-  { name: "Dashboard", link: "/dashboard", icon: FiMenu },
-  { name: "Monitors", link: "/monitors", icon: FiBarChart2 },
-  { name: "Reports", link: "/reports", icon: FiEdit },
-  { name: "Settings", link: "/settings", icon: FiSettings },
+  {
+    name: "Dashboard",
+    link: "/dashboard",
+    id: "tour-dashboard",
+    content: "Dashboard Here",
+    icon: FiMenu,
+  },
+  {
+    name: "Monitors",
+    link: "/monitors",
+    id: "tour-monitors",
+    content: "Monitors Here",
+    icon: FiBarChart2,
+  },
+  {
+    name: "Reports",
+    link: "/reports",
+    id: "tour-reports",
+    content: "Reports Here",
+    icon: FiEdit,
+  },
+  {
+    name: "Settings",
+    link: "/settings",
+    id: "tour-settings",
+    content: "Settings Here",
+    icon: FiSettings,
+  },
 ]
 
 export default function Nav() {
+  console.log(
+    nav.map(({ id, content }) => {
+      return { target: `.${id}`, content }
+    }),
+  )
   return (
     <>
       <div
@@ -25,6 +55,26 @@ export default function Nav() {
           minHeight: "100vh",
           backgroundColor: colors.brand,
         }}>
+        <Joyride
+          run={true}
+          scrollToFirstStep={true}
+          showProgress={true}
+          showSkipButton={true}
+          styles={{
+            options: {
+              arrowColor: "#e3ffeb",
+              backgroundColor: "#e3ffeb",
+              overlayColor: "rgba(79, 26, 0, 0.4)",
+              primaryColor: "#000",
+              textColor: "#004a14",
+              width: 900,
+              zIndex: 1000,
+            },
+          }}
+          step={nav.map(({ id, content }) => {
+            return { target: id, content }
+          })}
+        />
         <Link href="/index">
           <img
             src="../static/textlogo.png"
@@ -39,10 +89,12 @@ export default function Nav() {
               marginLeft: 10,
             },
           }}>
-          {nav.map(({ name, link, icon: Icon }) => {
+          {nav.map(({ name, link, id, icon: Icon }) => {
             return (
               <Link href={link}>
                 <div
+                  id={id}
+                  className={id}
                   css={{
                     padding: "10px 25px",
                     cursor: "pointer",
