41c8a468a55cfef2faad795aaa49be372456dd53
Maks <maks112v@gmail.com>
2019-09-03 00:41:28 +0300

    prismic data

---
+++ services/prismicData.js
@@ -0,0 +1,24 @@
+const Prismic = require("prismic-javascript")
+
+const API_ENDPOINT = `https://welldone-dashboard.cdn.prismic.io/api/v2`
+
+async function getDoc(pageName) {
+  const api = await Prismic.api(API_ENDPOINT)
+  const doc = await api.getSingle(pageName)
+  return doc
+}
+
+async function getDocs(docType) {
+  const api = await Prismic.api(API_ENDPOINT)
+  const docs = await api.query(
+    Prismic.Predicates.at("document.type", docType),
+    { pageSize: 1000 },
+  )
+  return docs
+}
+
+module.exports = {
+  API_ENDPOINT,
+  getDoc,
+  getDocs,
+}
