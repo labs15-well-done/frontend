b671150a55862d9f3daaa781b85dfba445956bfc
Maks <maks112v@gmail.com>
2019-09-02 23:47:22 +0300

    PLEASE get prettier

--- pages/settings.js
+++ pages/settings.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Nav from '../components/Nav';
+import React from "react"
+import Nav from "../components/Nav"
 
 export default function settings() {
-    return (
-        <>
-            <div className='container'>
-                <h2>Settings Page</h2>
-            </div>
-            <style jsx>{`
-            .container {
-                display: flex;
-            }
-            h2 {
-                margin-left: 10px;
-              }
-    `}</style>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <h2>Settings Page</h2>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+        }
+        h2 {
+          margin-left: 10px;
+        }
+      `}</style>
+    </>
+  )
 }
