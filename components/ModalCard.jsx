71f5304851dc0948bd6464114f3d3bdd65da5260
jkeslar21 <diamondroad91@gmail.com>
2019-09-03 10:14:31 -0700

    modal styles - color coated fucntional-unknown-nonfunc statuses

--- components/ModalCard.jsx
+++ components/ModalCard.jsx
@@ -6,11 +6,13 @@ export default function ModalCard({pump}) {
         <div>
             <p>Sensor ID: {pump.id}</p>
             <p>Total Water-Flow: {pump.statuses && pump.statuses[13].total}</p>
-            <p>Current Status: {pump.status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null}</p>
+            <p>Current Status:<span css={pump.status === 0 ? { color: "#f44336"} : pump.status === 1 ? { color: "#FFAD34"} : pump.status === 2 ? { color: "#01c000"} : null }> {pump.status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null}</span></p>
+
+            {/* <p>Current Status:<span css={pump.status === 0 ? { color: `${success}`} : pump.status === 1 ? { color: `${unknown}`} : pump.status === 2 ? { color: `${nonFunctional}`} : null }> {pump.status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null}</span></p> */}
             <p>3-Day Spread: <ul>
-            <li>{pump.dates && pump.dates[12]} {pump.statuses ? pump.statuses[12].status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null : 'Connection Failure'}</li>
-            <li>{pump.dates && pump.dates[11]} {pump.statuses ? pump.statuses[11].status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null : 'Connection Failure'}</li>
-            <li>{pump.dates && pump.dates[10]} {pump.statuses ? pump.statuses[10].status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null : 'Connection Failure'}</li>
+            <li>{pump.dates && pump.dates[12]} <span css={pump.statuses ? pump.statuses[12].status === 0 ? { color: "#f44336"} : pump.statuses[12].status === 1 ? { color: "#FFAD34"} : pump.statuses[12].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[12].status === 0 ? 'Non-Functional' : pump.statuses[12].status === 1 ? 'Unknown' : pump.statuses[12].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li>
+            <li>{pump.dates && pump.dates[11]} <span css={pump.statuses ? pump.statuses[11].status === 0 ? { color: "#f44336"} : pump.statuses[11].status === 1 ? { color: "#FFAD34"} : pump.statuses[11].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[11].status === 0 ? 'Non-Functional' : pump.statuses[11].status === 1 ? 'Unknown' : pump.statuses[11].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li>
+            <li>{pump.dates && pump.dates[10]} <span css={pump.statuses ? pump.statuses[10].status === 0 ? { color: "#f44336"} : pump.statuses[10].status === 1 ? { color: "#FFAD34"} : pump.statuses[10].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[10].status === 0 ? 'Non-Functional' : pump.statuses[10].status === 1 ? 'Unknown' : pump.statuses[10].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li>
             </ul></p>
         </div>
     )
