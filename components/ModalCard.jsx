345d45facc12570158c6ceb2548e51dba80167cc
jkeslar21 <diamondroad91@gmail.com>
2019-09-03 12:23:48 -0700

    sensor id became id and alignment was put on right, subject to change

--- components/ModalCard.jsx
+++ components/ModalCard.jsx
@@ -9,23 +9,12 @@ import {
 export default function ModalCard({pump}) {
 
     return (
-        <div>
-            <p>Sensor ID: {pump.id}</p>
-            <p>Total Water-Flow: {pump.statuses && pump.statuses[13].total}</p>
+        <div css={{ display: 'flex', flexDirection: 'column'}}>
+            <p css={{ alignSelf: 'right', fontWeight: 'bolder'}}>ID #{pump.id}</p>
+            {/* <p>Total Water-Flow: {pump.statuses && pump.statuses[13].total}</p> */}
             <p css={{ fontWeight: 'bolder'}}>Statuses</p>
-            {/* text */}
-            {/* <p><span css={{ textDecoration: 'underline'}} >Current:</span><span css={pump.status === 0 ? { color: "#f44336"} : pump.status === 1 ? { color: "#FFAD34"} : pump.status === 2 ? { color: "#01c000"} : null }> {pump.status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null}</span></p> */}
-            {/* icons */}
             <p><span css={{ textDecoration: 'underline'}} >Current:</span><span css={pump.status === 0 ? { color: "#f44336"} : pump.status === 1 ? { color: "#FFAD34"} : pump.status === 2 ? { color: "#01c000"} : null }> {pump.status === 0 ? <FiAlertCircle /> : pump.status === 1 ? <FiHelpCircle /> : pump.status === 2 ? <FiCheckCircle /> : null}</span></p>
-
-
-            {/* <p>Current Status:<span css={pump.status === 0 ? { color: `${success}`} : pump.status === 1 ? { color: `${unknown}`} : pump.status === 2 ? { color: `${nonFunctional}`} : null }> {pump.status === 0 ? 'Non-Functional' : pump.status === 1 ? 'Unknown' : pump.status === 2 ? 'Functional' : null}</span></p> */}
             <p><span css={{ textDecoration: 'underline'}}>3-Day Spread: </span><ul>
-           {/* text */}
-            {/* <li>{pump.dates && pump.dates[12]} <span css={pump.statuses ? pump.statuses[12].status === 0 ? { color: "#f44336"} : pump.statuses[12].status === 1 ? { color: "#FFAD34"} : pump.statuses[12].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[12].status === 0 ? 'Non-Functional' : pump.statuses[12].status === 1 ? 'Unknown' : pump.statuses[12].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li>
-            <li>{pump.dates && pump.dates[11]} <span css={pump.statuses ? pump.statuses[11].status === 0 ? { color: "#f44336"} : pump.statuses[11].status === 1 ? { color: "#FFAD34"} : pump.statuses[11].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[11].status === 0 ? 'Non-Functional' : pump.statuses[11].status === 1 ? 'Unknown' : pump.statuses[11].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li>
-            <li>{pump.dates && pump.dates[10]} <span css={pump.statuses ? pump.statuses[10].status === 0 ? { color: "#f44336"} : pump.statuses[10].status === 1 ? { color: "#FFAD34"} : pump.statuses[10].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[10].status === 0 ? 'Non-Functional' : pump.statuses[10].status === 1 ? 'Unknown' : pump.statuses[10].status === 2 ? 'Functional' : null : 'Connection Failure'}</span></li> */}
-            {/* icons */}
             <li>{pump.dates && pump.dates[12]} <span css={pump.statuses ? pump.statuses[12].status === 0 ? { color: "#f44336"} : pump.statuses[12].status === 1 ? { color: "#FFAD34"} : pump.statuses[12].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[12].status === 0 ? <FiAlertCircle /> : pump.statuses[12].status === 1 ? <FiHelpCircle /> : pump.statuses[12].status === 2 ? <FiCheckCircle /> : null : 'Connection Failure'}</span></li>
             <li>{pump.dates && pump.dates[11]} <span css={pump.statuses ? pump.statuses[11].status === 0 ? { color: "#f44336"} : pump.statuses[11].status === 1 ? { color: "#FFAD34"} : pump.statuses[11].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[11].status === 0 ? <FiAlertCircle /> : pump.statuses[11].status === 1 ? <FiHelpCircle /> : pump.statuses[11].status === 2 ? <FiCheckCircle /> : null : 'Connection Failure'}</span></li>
             <li>{pump.dates && pump.dates[10]} <span css={pump.statuses ? pump.statuses[10].status === 0 ? { color: "#f44336"} : pump.statuses[10].status === 1 ? { color: "#FFAD34"} : pump.statuses[10].status === 2 ? { color: "#01c000"} : null : '' }> {pump.statuses ? pump.statuses[10].status === 0 ? <FiAlertCircle /> : pump.statuses[10].status === 1 ? <FiHelpCircle /> : pump.statuses[10].status === 2 ? <FiCheckCircle /> : null : 'Connection Failure'}</span></li>
