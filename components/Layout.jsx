bc497928df685b00e35e61f1b4131d008719841c
Maks <maks112v@gmail.com>
2019-09-04 22:54:51 +0300

    pushed changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,13 +7,18 @@ const StoreContext = createContext({
 export const useStore = () => useContext(StoreContext)
 
 export default function Layout({ children }) {
-  const [store, setstore] = useState({ nav: false })
+  const [store, setstore] = useState({ nav: false, tour: false })
 
   const toggleNav = () => {
     setstore({ ...store, nav: !store.nav })
   }
+
+  const toggleTour = () => {
+    setstore({ ...store, tour: !store.tour })
+  }
+
   return (
-    <StoreContext.Provider value={{ store, setstore, toggleNav }}>
+    <StoreContext.Provider value={{ store, setstore, toggleNav, toggleTour }}>
       <div css={{ display: "flex" }}>{children}</div>
     </StoreContext.Provider>
   )
