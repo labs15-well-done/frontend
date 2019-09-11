830a6fe48cd06dbc27497716363bc79dde4a29f2
Maks <maks112v@gmail.com>
2019-09-11 11:27:36 +0300

    reports page

--- pages/reports.js
+++ pages/reports.js
@@ -33,75 +33,72 @@ export default function Reports({ pumps }) {
         }
       />
       <div css={{ padding: "0 20px", maxWidth: 1240, margin: "0 auto" }}>
-        {pumps
-          .filter(pump => filter[pump.status])
-          .map(pump => {
-            return (
-              <details
+        {pumps.sort().map(pump => {
+          return (
+            <details
+              css={{
+                padding: "20px 30px",
+                backgroundColor: "white",
+                borderRadius: 10,
+                margin: "10px 0",
+              }}>
+              <summary
                 css={{
-                  padding: "20px 30px",
-                  backgroundColor: "white",
-                  borderRadius: 10,
-                  margin: "10px 0",
+                  display: "flex",
+                  alignItems: "center",
+                  outline: "none",
+                  cursor: "pointer",
+                  "::-webkit-details-marker": {
+                    display: "none",
+                  },
                 }}>
-                <summary
-                  css={{
-                    display: "flex",
-                    alignItems: "center",
-                    outline: "none",
-                    cursor: "pointer",
-                    "::-webkit-details-marker": {
-                      display: "none",
-                    },
-                  }}>
-                  <div css={{ marginRight: 10 }}>
-                    <span css={{ color: pumpStyles.status[pump.status].color }}>
-                      {" "}
-                      {pumpStyles.status[pump.status].icon}
-                    </span>
-                  </div>
-                  <div css={{ flexGrow: 1 }}>{pump.id}</div>
-                  <div css={{ textAlign: "right" }}>
-                    <div css={{ display: "flex" }}>
-                      {pump.statuses ? (
-                        Object.keys(pump.statuses).map(date => {
-                          const status = pump.statuses[date].status
-                          return (
-                            <div
-                              css={{
-                                width: 10,
-                                height: 20,
-                                margin: "0 2px",
-                                borderRadius: 3,
-                                backgroundColor:
-                                  pumpStyles.status[status].color,
-                                [breakingPoints.sm]: {
-                                  width: 5,
-                                },
-                              }}></div>
-                          )
-                        })
-                      ) : (
-                        <h3 css={{ margin: 0, fontSize: 16 }}>No Data</h3>
-                      )}
-                    </div>
-                    <p
-                      css={{
-                        fontSize: 12,
-                        margin: "5px 0 0",
-                        color: colors.darkText,
-                        opacity: 0.8,
-                      }}>
-                      Click For More Info
-                    </p>
-                  </div>
-                </summary>
-                <div>
-                  <h3>More info and charts</h3>
+                <div css={{ marginRight: 10 }}>
+                  <span css={{ color: pumpStyles.status[pump.status].color }}>
+                    {" "}
+                    {pumpStyles.status[pump.status].icon}
+                  </span>
                 </div>
-              </details>
-            )
-          })}
+                <div css={{ flexGrow: 1 }}>{pump.id}</div>
+                <div css={{ textAlign: "right" }}>
+                  <div css={{ display: "flex" }}>
+                    {pump.statuses ? (
+                      Object.keys(pump.statuses).map(date => {
+                        const status = pump.statuses[date].status
+                        return (
+                          <div
+                            css={{
+                              width: 10,
+                              height: 20,
+                              margin: "0 2px",
+                              borderRadius: 3,
+                              backgroundColor: pumpStyles.status[status].color,
+                              [breakingPoints.sm]: {
+                                width: 5,
+                              },
+                            }}></div>
+                        )
+                      })
+                    ) : (
+                      <h3 css={{ margin: 0, fontSize: 16 }}>No Data</h3>
+                    )}
+                  </div>
+                  <p
+                    css={{
+                      fontSize: 12,
+                      margin: "5px 0 0",
+                      color: colors.darkText,
+                      opacity: 0.8,
+                    }}>
+                    Click For More Info
+                  </p>
+                </div>
+              </summary>
+              <div>
+                <h3>More info and charts</h3>
+              </div>
+            </details>
+          )
+        })}
       </div>
     </>
   )
