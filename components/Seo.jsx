b3931d30e3a89c31bf74237b55fd7c44da64c596
Maks <maks112v@gmail.com>
2019-09-12 01:29:52 +0300

    current status

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import Head from "next/head"
+import { RichText } from "prismic-reactjs"
 
-export default function Seo({ title, description, image }) {
+export default function Seo({ data }) {
   const url = "https://dashboard.welldone.org"
   const author = "Lambda School Labs15"
   const handle = "@welldone"
+
+  const title = RichText.asText(data.title)
+  const desc = RichText.asText(data.description)
+  const image = data.image && data.image.url
 
   return (
     <Head>
@@ -16,18 +21,14 @@ export default function Seo({ title, description, image }) {
       {title && <meta property="og:title" content={title} key="og:title" />}
       <meta property="og:url" content={url} key="og:url" />
       <meta property="og:type" content="website" key="og:type" />
-      {description && (
-        <meta property="og:description" content={description} key="og:desc" />
-      )}
+      {desc && <meta property="og:description" content={desc} key="og:desc" />}
       {image && <meta property="og:image" content={image} key="og:image" />}
       <meta property="og:image:type" content="image/png" key="og:image:type" />
       <meta property="og:image:width" content="800" key="og:image:width" />
 
       {handle && <meta name="twitter:site" content={handle} key="tw:site" />}
       {title && <meta name="twitter:title" content={title} key="tw:title" />}
-      {description && (
-        <meta name="twitter:description" content={description} key="tw:desc" />
-      )}
+      {desc && <meta name="twitter:description" content={desc} key="tw:desc" />}
       {image && <meta name="twitter:image" content={image} key="tw:image" />}
       <meta name="twitter:card" content="summary_large_image" key="tw:card" />
     </Head>
