bd14a89d323a6dd5ca445a450fff921d25439cf0
JaySebagh <jaysebagh@gmail.com>
2019-09-18 13:42:29 -0700

    Joshua Keslar and I pulled the current selected pump status information. Converted object of statuses to an array of objects.

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,12 +2,17 @@ import React from "react"
 import ModalCard from "./ModalCard"
 
 export default function Modal({ pumps, modalId }) {
-  const selectedPump = modalId ? pumps.find(pump => pump.id === modalId) : null
+  const selectedPump = modalId ? pumps.find(pump => pump.id === modalId) : null;
+  const pumpToArr = modalId && selectedPump.statuses ? Object.keys(selectedPump.statuses).map(i => selectedPump.statuses[i]) : null;
+
+  console.log("an array?", pumpToArr);
+  // console.log("help:", selectedPump);
   return (
     <div>
       {modalId ? (
         <div>
           <h3>Pump #{selectedPump.id}</h3>
+          {/* <p>{selectedPump.statuses[1]}</p> */}
         </div>
       ) : (
         <h3>Select a Pump</h3>
