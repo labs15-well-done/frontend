7b733a09363fb581cae42da167e5fc6e9616fb45
JaySebagh <jaysebagh@gmail.com>
2019-09-18 14:40:35 -0700

    updated styling and got icon status displayed for current

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,7 +1,28 @@
 import React from "react"
 import ModalCard from "./ModalCard"
+import {
+  FiEdit,
+  FiHelpCircle,
+  FiAlertCircle,
+  FiCheckCircle,
+} from "react-icons/fi"
+import { colors } from "./Styles";
+
+const styles = {
+  0: {
+    color: colors.danger,
+    icon: <FiAlertCircle color={colors.danger}/>
+  },1: {
+    color: colors.orange,
+    icon: <FiHelpCircle color={colors.orange} />
+  },2: {
+    color: colors.success,
+    icon: <FiCheckCircle color={colors.success} />
+  },
+}
 
 export default function Modal({ pumps, modalId }) {
+
   const selectedPump = modalId ? pumps.find(pump => pump.id === modalId) : null;
   const pumpToArr = modalId && selectedPump.statuses ? Object.keys(selectedPump.statuses).map(i => selectedPump.statuses[i]) : null;
 
@@ -11,14 +32,14 @@ export default function Modal({ pumps, modalId }) {
     <div>
       {modalId ? (
         <div>
-          <h3>Pump #{selectedPump.id}</h3>
-          <p>Current: {pumpToArr[13].status}</p>
+          <h3 css={{color:styles[selectedPump.status].color }}>Pump #{selectedPump.id}</h3>
+          <p>Current: {styles[selectedPump.status].icon}</p>
           <p>3-Day Spread:</p>
-          <ul>
+          {/* <ul>
             <li>{pumpToArr[12].status}</li>
             <li>{pumpToArr[11].status}</li>
             <li>{pumpToArr[10].status}</li>
-          </ul>
+          </ul> */}
         </div>
       ) : (
         <h3>Select a Pump</h3>
