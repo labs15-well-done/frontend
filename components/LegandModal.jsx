1a38c05f2416bae84858e1240399282eb1f17e49
Andrew Brudnak <brudnak@icloud.com>
2019-09-18 17:24:06 -0700

    :nail_care: Icon Legend looking better

--- components/LegandModal.jsx
+++ components/LegandModal.jsx
@@ -1,26 +1,56 @@
 import React from "react"
 import Popup from "reactjs-popup"
 import styled from "@emotion/styled"
-import { FiInfo } from "react-icons/fi"
+import { FiInfo, FiX } from "react-icons/fi"
 import { colors } from "./Styles"
+
+const mapPins = {
+  status: {
+    functional: "../static/success.svg",
+    unknown: "../static/unknown.svg",
+    nonFunctional: "../static/error.svg",
+  },
+}
 
 const Wrapper = styled("div")`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
 
-  & button {
-    border: none;
-    border-radius: 50%;
+  & span {
     margin-right: 5%;
+    font-size: 1.6rem;
     cursor: pointer;
-    transition: 0.3s;
-    background-color: ${colors.brand};
-    box-shadow: 7px 10px 12px -5px rgba(0, 0, 0, 0.56);
-    color: white;
-    outline: none;
-    border: 3px solid ${colors.brand};
-    font-weight: 600;
+    color: ${colors.brand};
+    font-weight: lighter;
+  }
+`
+const Legand = styled("div")`
+  h4 {
+    text-align: center;
+    font-size: 1.1rem;
+    text-transform: uppercase;
+    word-spacing: 9px;
+    letter-spacing: 2px;
+    margin: 4px 0px;
+    color: #2a2a32ed;
+  }
+`
+
+const Row = styled("div")`
+  display: flex;
+  & p {
+    margin-left: 20px;
+  }
+`
+const Close = styled("div")`
+  display: flex;
+  justify-content: flex-end;
+
+  & svg {
+    cursor: pointer;
+    color: #2a2a32ed;
+    font-size: 1.6rem;
   }
 `
 
@@ -29,18 +59,51 @@ const LegandModal = () => {
     <Wrapper>
       <Popup
         trigger={
-          <button>
+          <span>
             <FiInfo />
-          </button>
+          </span>
         }
         position="left top">
         {close => (
-          <>
-            <h2>Icon Legend</h2>
-            <a className="close" onClick={close}>
-              &times;
-            </a>
-          </>
+          <Legand>
+            <h4>Icon Legend</h4>
+
+            <Row>
+              <img
+                src={mapPins.status.functional}
+                width={"24px"}
+                css={{ zIndex: 2 }}
+                onClick={() => setModalId(pump.id)}
+                alt=""
+              />
+              <p>Functional</p>
+            </Row>
+
+            <Row>
+              <img
+                src={mapPins.status.unknown}
+                width={"24px"}
+                css={{ zIndex: 2 }}
+                onClick={() => setModalId(pump.id)}
+                alt=""
+              />
+              <p>Unknown</p>
+            </Row>
+
+            <Row>
+              <img
+                src={mapPins.status.nonFunctional}
+                width={"24px"}
+                css={{ zIndex: 2 }}
+                onClick={() => setModalId(pump.id)}
+                alt=""
+              />
+              <p>Non-Functional</p>
+            </Row>
+            <Close>
+              <FiX onClick={close} />
+            </Close>
+          </Legand>
         )}
       </Popup>
     </Wrapper>
