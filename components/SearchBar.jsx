672b356a06c06191011d27dfe9d5a2c16355bea6
jkeslar21 <diamondroad91@gmail.com>
2019-09-24 12:11:42 -0700

    search icon

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { colors } from './Styles'
 
 export default function SearchBar({ searchHandler }) {
     return (
-        <div>
+        <div css={{ display: 'flex', alignItems: 'center'}} >
+            <FaSearch css={{marginRight: '10px', color: [colors.brand]}} />
             <input type='text' placeholder='search...' onChange={searchHandler} />
         </div>
     )
