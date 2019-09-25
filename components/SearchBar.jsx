683249ce762c6e5b684c35029a4286218ce240e3
jkeslar21 <diamondroad91@gmail.com>
2019-09-25 09:36:23 -0700

    searchbar styles

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -6,7 +6,7 @@ export default function SearchBar({ searchHandler }) {
     return (
         <div css={{ display: 'flex', alignItems: 'center'}} >
             <FaSearch css={{marginRight: '10px', color: [colors.brand]}} />
-            <input type='text' placeholder='search...' onChange={searchHandler} />
+            <input type='text' placeholder='search...' onChange={searchHandler} css={{ border: '1px solid #DDE1E5', outline: 'none', color: [colors.brand], fontWeight: 'bold', textAlign: 'center', borderRadius: '10px'}} />
         </div>
     )
 }
